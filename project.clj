ac7a832e09a94380361793f94cd2ed06128ca5ad
Adrian Mowat <adrian.mowat@gmail.com>
2012-07-15 12:18:25 +0100

    Created new lein2 project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clam "0.1.0-SNAPSHOT"
+  :description "Data description language for parsing text streams"
+  :url "https://github.com/mowat27/clam"
+  :license {:name "MIT Licence"
+            :url "http://copyfree.org/licenses/mit/license.txt"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[midje "1.4.0"]])
