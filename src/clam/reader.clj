c35e187ba4a0af0eebdd6612fa412edf642379d2
Adrian Mowat <adrian.mowat@gmail.com>
2012-07-15 23:37:48 +0100

    added next-field function that we can plug in to get row

--- src/clam/reader.clj
+++ src/clam/reader.clj
@@ -1,6 +1,15 @@
-(ns clam.reader)
+(ns clam.reader
+  (:import java.io.StringReader))
 
-(defn tst [] "hello world")
+(defn next-field
+  ([reader delimiter] (next-field reader delimiter []))
+  ([reader delimiter result]
+    (let [byte-code (.read reader) character (or (= -1 byte-code) (char byte-code))]
+      (cond
+        (= -1 byte-code) nil
+        (= delimiter (str character)) (apply str (reverse result))
+        :else (recur reader delimiter (cons character result))
+      ))))
 
 (defn read-row [text delimiter]
 
@@ -11,5 +20,4 @@
         (cons ch-str result))))
 
   [(apply str (reverse (reduce build-field-vec [] text)))]
-
 )
--- test/clam/reader/reader_test.clj
+++ test/clam/reader/reader_test.clj
@@ -1,9 +1,16 @@
 (ns clam.reader.reader_test
   (:use midje.sweet
-        clam.reader))
+        clam.reader)
+  (:import java.io.StringReader))
+
+(defn reader [] (new StringReader "foo,bar,"))
+
+(facts "about next-field"
+  (next-field (reader) ",") => "foo"
+  (next-field (reader) "|") => nil)
 
 (facts "about read-row"
   (read-row "f1|" "|") => ["f1"]
   (read-row "f1," ",") => ["f1"]
-  ; (read-row "f1,f2," ",") => ["f1"]
+  ; (read-row "f1,f2," ",") => ["f1" "f2"]
 )
